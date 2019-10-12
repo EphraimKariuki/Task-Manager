2da77baab0f3f6341f352425ac79246360f4dd9f
EphraimKariuki <ephraimkabuikariuki@gmail.com>
2019-10-12 15:20:15 +0100

    first push

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLalchemy
+
+app = Flask(__name__)
+
+#configuring the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#initialise database
+db = SQLalchemy(app)
+
+class todo(db.model)
+    id = db.column(db.integer, primary_key=True)
+    content = db.column(db.string(200), nullable=flase)
+    completed = db.column(db.integer, defult=0)
+    date_created = db.column(db.DateTime, default=datetime.utcnow)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ enviroment/lib/python3.7/sre_compile.py
@@ -0,0 +1,1 @@
+/home/ephraim/anaconda3/lib/python3.7/sre_compile.py
